b0b8f787426494237bd98ae4aa9292827fd7e33c
Hasnain7212 <hasnainrazalakhani7212@gmail.com>
2024-11-16 08:14:27 +0530

    error fix

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-// components/index.js
-export {DynamicNav} from './DynamicNav';
-export {DynamicTable} from './DynamicTable';
-export {DynamicForm} from './DynamicForm';
-export {DynamicChart} from './DynamicChart';
